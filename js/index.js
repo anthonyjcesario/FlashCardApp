1bb83313733d40f80c1586dd0070ab4d4ba3e7ca
Anthony Cesario <ajcesario4746@gmail.com>
2025-09-01 13:29:59 -0400

    Creating array to store random background colors for deck covers.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
+const backgroundColors = [
+    "linear-gradient(to right bottom, #373F36, #4B2127)",
+    "linear-gradient(to right bottom, #4B2127, #C78805)",
+    "linear-gradient(to right bottom, #b8520a, #779e7f)"
+];
+
 const createNewButton = document.querySelector("#createNewButton");
 createNewButton.addEventListener("click", () => {
+
     deckTitlePrompt = prompt("Deck Title: ");
     const deckContainer = document.querySelector(".deckContainer");
 
     const deck = document.createElement("div");
     deck.classList.add("deck");
+    const randomIndex = Math.floor(Math.random() * backgroundColors.length);
+    deck.style.backgroundImage = backgroundColors[randomIndex];
 
     const deckTitle = document.createElement("h4");
     deckTitle.innerHTML = deckTitlePrompt;
