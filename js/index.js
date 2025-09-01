5873bacf444ad8fa0798d8d8f9418919b8615648
Anthony Cesario <ajcesario4746@gmail.com>
2025-09-01 13:20:26 -0400

    Adding dom manipulation stuff

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 const createNewButton = document.querySelector("#createNewButton");
 createNewButton.addEventListener("click", () => {
+    const deckContainer = document.querySelector(".deckContainer");
 
+    const deck = document.createElement("div");
+    deck.classList.add("deck");
+
+    deckContainer.appendChild(deck);
 });
