dd84816cf4a5bec202516190030180b690292a6e
Anthony Cesario <ajcesario4746@gmail.com>
2025-09-01 13:22:04 -0400

    Adding dom manipulation stuff / adding decks to deck container

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const createNewButton = document.querySelector("#createNewButton");
 createNewButton.addEventListener("click", () => {
+    deckTitlePrompt = prompt("Deck Title: ");
     const deckContainer = document.querySelector(".deckContainer");
 
     const deck = document.createElement("div");
     deck.classList.add("deck");
+
+    const deckTitle = document.createElement("h4");
+    deckTitle.innerHTML = deckTitlePrompt;
+
+    deck.appendChild(deckTitle);
 
     deckContainer.appendChild(deck);
 });
